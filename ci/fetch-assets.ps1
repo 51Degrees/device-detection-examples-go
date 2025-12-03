f4887ff8fafbb0272da89efceb48d89315f5c8da
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-12-03 05:31:29 +0200

    Merged Pull Request '#64 common-fetch-assets->main: ci: use common fetch-assets script'

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,27 +1,8 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName
-)
+$ErrorActionPreference = "Stop"
 
-$assets = New-Item -ItemType Directory -Path assets -Force
-$assetsDestination = "$RepoName"
-$file = "51Degrees-LiteV4.1.hash"
+$assets = "51Degrees-LiteV4.1.hash", "20000 User Agents.csv", "20000 Evidence Records.yml"
 
-$downloads = @{
-    "51Degrees-LiteV4.1.hash" = {Invoke-WebRequest -Uri "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash" -OutFile $assets/$file}
-    "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -OutFile $assets/$file}
-    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml" -OutFile $assets/$file}
+./steps/fetch-assets.ps1 -Assets $assets
+foreach ($asset in $assets) {
+    New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/$asset" -Path "$PSScriptRoot/../$asset"
 }
-
-foreach ($file in $downloads.Keys) {
-    if (!(Test-Path $assets/$file)) {
-        Write-Output "Downloading $file"
-        Invoke-Command -ScriptBlock $downloads[$file]
-    } else {
-        Write-Output "'$file' exists, skipping download"
-    }
-}
-
-New-Item -ItemType SymbolicLink -Force -Target "$assets/51Degrees-LiteV4.1.hash" -Path "$assetsDestination/51Degrees-LiteV4.1.hash"
-New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 User Agents.csv" -Path "$assetsDestination/20000 User Agents.csv"
-New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 Evidence Records.yml" -Path "$assetsDestination/20000 Evidence Records.yml"
